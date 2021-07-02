3d51cff0ad3cd86ec27e10e0a4201c0058041d70
Artur <59160676+MrAviator93@users.noreply.github.com>
2021-07-02 12:18:55 +0100

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,11 +1,20 @@
 #! /usr/bin/python3
 
 import os
+import platform
 
 print( "Configuring the project .... " )
 
 configure_flags = ""
 
+pl_name = platform.system()
+print( pl_name )
+
+if pl_name == "Linux":
+    print( platform.linux_distribution()[ 0 ] )    
+
+# TODO: This script must take into account to which platform we are building!
+    
 if os.path.exists( "Build/" ) != True:
     os.mkdir( "Build" )
     os.chdir( "Build" )
